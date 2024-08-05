a165bfd720afed6f32cff1251f4fb89d92b8dd4e
Jedyokey <jedyokey29@gmail.com>
2024-08-05 03:02:02 +0000

    react code update

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Food from './components/Food'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Food />
+      <Food />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
